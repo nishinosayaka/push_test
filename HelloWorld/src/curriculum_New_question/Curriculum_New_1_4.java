95ee6f01c5998e9b122fa0dd6586f15e6ef184a8
nishinosayaka <sshanben13@gmail.com>
2023-04-12 23:13:51 +0900

    課題1.4

---
+++ HelloWorld/src/curriculum_New_question/Curriculum_New_1_4.java
@@ -0,0 +1,45 @@
+package curriculum_New_question;
+
+public class Curriculum_New_1_4 {
+
+	
+	public static void main(String[] args) {
+//		Q1.Q2
+		byte b= 0 ;
+		short s = 0;
+		int i = 0;
+		long l = 0;
+		float f = 0.0f;
+		double d = 0.0d;	
+		char c = '\u0000';
+		String str = null;
+		boolean boo = false;
+		
+//		Q3
+		b = 10;
+		s = 100;
+		i = 1000;
+		l = 10000;
+		f = 9.5f;
+		d = 10.5d;
+		c = 'a';
+		str = "ハロー";
+		boo = true;
+		
+//		Q4
+		System.out.println(l + i + s + b);
+		System.out.println(f + d);
+		System.out.println(c + str + boo);
+		System.out.println(l+i+s+b+f+d);
+		System.out.println(b*s*i*l);
+		System.out.println(d/s);
+		System.out.println(b-s);
+//		Q5
+		int num = 20;
+ 		int num1 = 23;
+ 		System.out.println("ハローJAVA" + (num + num1));
+ 		 
+		
+		
+	}
+}
