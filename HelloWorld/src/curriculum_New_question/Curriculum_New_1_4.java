39b10366cf5f34a424622fd11ad7ffc9a65a788b
nishinosayaka <sshanben13@gmail.com>
2023-04-14 22:38:35 +0900

    課題1.4　Q1再修正

--- HelloWorld/src/curriculum_New_question/Curriculum_New_1_4.java
+++ HelloWorld/src/curriculum_New_question/Curriculum_New_1_4.java
@@ -1,8 +1,8 @@
 package curriculum_New_question;
 
-public class Curriculum_New_1_4 {
-//	Q1.下記9個をクラス変数として宣言のみしてください
-	//バイト型
+//Q1.下記9個をクラス変数として宣言のみしてください
+class test{
+//バイト型
 	static byte b;
 	//短整数型
 	static short s;
@@ -20,67 +20,71 @@ public class Curriculum_New_1_4 {
 	static String str;
 	//ブーリアン型 
 	static boolean boo;
-	
+}
+
+
+public class Curriculum_New_1_4 {
+
 	
 	public static void main(String[] args) {
 //		Q2 それぞれのクラス変数をローカル内でそれぞれの初期値を代入し初期化してください
 		//バイト型
-		b= 0 ;
+		test.b= 0 ;
 		//短整数型
-		s = 0;
+		test.s = 0;
 		//整数型
-		i = 0;
+		test.i = 0;
 		//長整数型
-		l = 0;
+		test.l = 0;
 		//単精度浮動小数点数型
-		f = 0.0f;
+		test.f = 0.0f;
 		//倍精度浮動小数点数型
-		d = 0.0d;	
+		test.d = 0.0d;	
 		//文字型
-		c = '\u0000';
+		test.c = '\u0000';
 		//文字列型
-		str = null;
+		test.str = null;
 		//ブーリアン型 
-		boo = false;
+		test.boo = false;
 		
 //		Q3初期化をしたそれぞれの変数に下記の値を代入してください
 	     // ・バイト型                 10
-		b = 10;
+		test.b = 10;
 	     // ・短整数型                 100
-		s = 100;
+		test.s = 100;
 	     // ・整数型                	 1000
-		i = 1000;
+		test.i = 1000;
 	     // ・長整数型                 10000
-		l = 10000;
+		test.l = 10000;
 	     // ・単精度浮動小数点数型   	 9.5
-		f = 9.5f;
+		test.f = 9.5f;
 	     // ・倍精度浮動小数点数型		 10.5
-		d = 10.5d;
+		test.d = 10.5d;
 	     // ・文字型                   a
-		c = 'a';
+		test.c = 'a';
 	     // ・文字列型              	 ハロー
-		str = "ハロー";
+		test.str = "ハロー";
 	     // ・ブーリアン型          	 true
-		boo = true;
+		test.boo = true;
 		
 	 	 
 	     // Q4 下記の通りにコンソール出力されるようにしてください
 	     // 必ず変数を使用すること
 	
 	     // 11110
-		System.out.println(l + i + s + b);
+		System.out.println(test.l +test. i + test.s + test.b);
 	     // 20.0
-		System.out.println(f + d);
+		System.out.println(test.f + test.d);
 	     // a ハロー true
-		System.out.println(c + str + boo);
+		System.out.println(test.c +test.str +test.boo);
 	     // 11130.0                    数字を全て足す
-		System.out.println(l+i+s+b+f+d);
+		System.out.println(test.l+test.i+test.s+test.b+test.f+test.d);
 	     // 10000000000                小数点以外の数字を全てかける
-		System.out.println(b*s*i*l);
+		System.out.println(test.b*test.s*test.i*test.l);
 	     // 0.105                      10.5割る100をする
-		System.out.println(d/s);
+		System.out.println(test.d/test.s);
 	     // -90                        10引く100をする
-		System.out.println(b-s);
+		System.out.println(test.b-test.s);
 		
 		
 //		Q5次のプログラムを実行すると「ハローJAVA2023」という結果が表示されます。
